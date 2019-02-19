8a9c73fc693638026631de4f77e3a09a4a8edc59
MattiMatt8 <momatthiasson@gmail.com>
2019-02-19 11:31:24 +0000

    Silaverkefni4 uppfært

--- skilaverkefni4.py
+++ skilaverkefni4.py
@@ -1,18 +1,19 @@
 # Matthías Ólafur
 
 # 2.
+print("\n--------- Dæmi 2 ---------\n")
 def linearLeit(listi,finna):
     for x in range(len(listi)):
         if listi[x] == finna:
             return x
     return -1
-
 listi = [8,5,3,7,1,9,2,6]
 print("Staða 1: ",linearLeit(listi,4))
 print("Staða 1: ",linearLeit(listi,2))
 
 
 # 3.
+print("\n--------- Dæmi 3 ---------\n")
 def binaryLeit(listi,finna):
     midja = len(listi)//2
     if midja == 0:
@@ -35,6 +36,7 @@ print("Binary",binaryLeit(listi,10))
 print("Binary",binaryLeit(listi,6))
 
 # 5.
+print("\n--------- Dæmi 5 ---------\n")
 def insertRett(listi,tala):
     if len(listi) == 0 or tala >= listi[len(listi)-1]:
         listi.append(tala)
@@ -71,7 +73,85 @@ print("Very nice26\t",listi)
 listi = insertRett(listi,1)
 print("Very nice1\t",listi)
 
-# tré
-#           5
-#       4       7
-#      3       6  8
+
+# 6
+print("\n--------- Dæmi 6 ---------\n")
+class Node:
+    def __init__(self,v):
+        self.value = v
+        self.left = None
+        self.right = None
+    def insert(self,v):
+        if self.value == v:
+            return False
+        elif self.value > v:
+            if self.left:
+                return self.left.insert(v)
+            else:
+                self.left = Node(v)
+                return True
+        else:
+            if self.right:
+                return self.right.insert(v)
+            else:
+                self.right = Node(v)
+                return True
+class Tree:
+    def __init__(self):
+        self.root = None
+    def insert(self,v):
+        if self.root:
+            return self.root.insert(v)
+        else:
+            self.root = Node(v)
+            return True
+
+def finna(tre,data):
+    if tre.root:
+        return finnaN(tre.root,data)
+    return False
+def finnaN(root,data):
+    if root.value and root.value == data:
+        return True
+    if root.left:
+        if finnaN(root.left,data):
+            return True
+    if root.right:
+        return finnaN(root.right,data)
+    return False
+print("\n------ Tré 1 ------\n")
+t = Tree()
+print("Insert 7:",t.insert(7))
+print("Insert 3:",t.insert(3))
+print("Insert 1:",t.insert(1))
+print("Insert 6:",t.insert(6))
+print("Insert 8:",t.insert(8))
+print("Insert 5:",t.insert(5))
+print("Insert 10:",t.insert(10))
+print("Insert 9:",t.insert(9))
+print("Insert 1:",t.insert(1))
+print("Insert 2:",t.insert(2))
+print()
+print("Finnst 7: ",finna(t,7))
+print("Finnst 3: ",finna(t,3))
+print("Finnst 1: ",finna(t,1))
+print("Finnst 2: ",finna(t,2))
+print("Finnst 6: ",finna(t,6))
+print("Finnst 5: ",finna(t,5))
+print("Finnst 8: ",finna(t,8))
+print("Finnst 9: ",finna(t,9))
+print("Finnst 10: ",finna(t,10))
+print()
+print("Finnst 0: ",finna(t,0))
+print("Finnst 12: ",finna(t,12))
+print("Finnst 4: ",finna(t,4))
+print()
+print("Finnst: ",finna(t,7))
+
+
+t2 = Tree()
+print()
+print("\n------ Tré 2 ------\n")
+print("Finnst 7: ",finna(t2,7))
+
+print("\n-------------------\n")
